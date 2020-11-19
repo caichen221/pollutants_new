666d60aff284fbee224aa14f3f87f83c53c90576
76775081@qq.com <5211314a>
2020-11-19 21:17:53 +0800

    1、springboot版本升级至2.4.0 2、servlet-api版本升级至4.0.1 3、删除了datasong-client-plus的jar包和相应功能暂时注释掉，因为与springboot2.4.0有冲突 4、升级后功能没有做全面测试，此版本不确定稳定

--- biz-base/src/main/java/com/iscas/base/biz/test/datasongplus/repository/AchievementsRepository.java
+++ biz-base/src/main/java/com/iscas/base/biz/test/datasongplus/repository/AchievementsRepository.java
@@ -1,20 +1,20 @@
-package com.iscas.base.biz.test.datasongplus.repository;
-
-import com.iscas.base.biz.test.datasongplus.domain.Achievements;
-import com.iscas.datasong.client.plus.repository.DetRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-/**
- * //TODO
- *
- * @author zhuquanwen
- * @vesion 1.0
- * @date 2020/3/2 10:53
- * @since jdk1.8
- */
-@Repository
-public interface AchievementsRepository extends DetRepository<Achievements, String> {
-    List<Achievements> findByAuthorCNLike(String name);
-}
+//package com.iscas.base.biz.test.datasongplus.repository;
+//
+//import com.iscas.base.biz.test.datasongplus.domain.Achievements;
+//import com.iscas.datasong.client.plus.repository.DetRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+///**
+// * //TODO
+// *
+// * @author zhuquanwen
+// * @vesion 1.0
+// * @date 2020/3/2 10:53
+// * @since jdk1.8
+// */
+////@Repository
+//public interface AchievementsRepository extends DetRepository<Achievements, String> {
+//    List<Achievements> findByAuthorCNLike(String name);
+//}
