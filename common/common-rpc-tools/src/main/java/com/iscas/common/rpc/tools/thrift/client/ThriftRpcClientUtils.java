90e9be4568e0ddcd53429cbc5d465d4db988bcb3
76775081@qq.com <5211314a>
2020-11-21 13:33:41 +0800

    common-thrift-tools中添加thrift使用流程 添加thrift工具类

---
+++ common/common-rpc-tools/src/main/java/com/iscas/common/rpc/tools/thrift/client/ThriftRpcClientUtils.java
@@ -0,0 +1,57 @@
+package com.iscas.common.rpc.tools.thrift.client;
+
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+/**
+ * //TODO
+ *
+ * @author zhuquanwen
+ * @vesion 1.0
+ * @date 2020/11/21 13:02
+ * @since jdk1.8
+ */
+public class ThriftRpcClientUtils {
+    private ThriftRpcClientUtils() {}
+
+    /**
+     * 获取transport
+     * 使用例子：
+     *        TTransport transport = null;
+     *         try {
+     *             transport = new TSocket("127.0.0.1", 6367, 2000);
+     *             // 协议要和服务端一致
+     *             TProtocol protocol = new TBinaryProtocol(transport);
+     *
+     *             RPCDateService.Client client = new RPCDateService.Client(protocol);
+     *             transport.open();
+     *             String result = client.getDate("张三");
+     *             System.out.println("Thrify client result =: " + result);
+     *         } catch (TTransportException e) {
+     *             e.printStackTrace();
+     *         } catch (TException e) {
+     *             e.printStackTrace();
+     *         } finally {
+     *             if (null != transport) {
+     *                 transport.close();
+     *             }
+     *         }
+     *
+     * @version 1.0
+     * @since jdk1.8
+     * @date 2020/11/21
+     * @param ip
+     * @param port
+     * @param timeout 超时时间
+     * @throws
+     * @return
+     */
+    public static TTransport getTransport(String ip, int port, long timeout) {
+        TTransport transport = new TSocket("127.0.0.1", 6367, 2000);
+        return transport;
+    }
+
+    public static void close(TTransport tTransport) {
+        tTransport.close();
+    }
+}
---
+++ common/common-rpc-tools/src/main/java/com/iscas/common/rpc/tools/thrift/server/ThriftRpcServerUtils.java
@@ -0,0 +1,45 @@
+package com.iscas.common.rpc.tools.thrift.server;
+
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * //TODO
+ *
+ * @author zhuquanwen
+ * @vesion 1.0
+ * @date 2020/11/21 12:47
+ * @since jdk1.8
+ */
+public class ThriftRpcServerUtils {
+    private ThriftRpcServerUtils() {}
+
+
+    /**
+     * 发布服务
+     *
+     * @version 1.0
+     * @since jdk1.8
+     * @date 2020/11/21
+     * @param tprocessor  TProcessor tprocessor = new RPCDateService.Processor<RPCDateService.Iface>( new RPCDateServiceImpl());
+     * @param port 对外发布的端口
+     * @throws
+     * @return void
+     */
+    public static <T extends TServer> void serve(TProcessor tprocessor, int port) throws TTransportException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        TServerSocket serverTransport = new TServerSocket(port);
+        TServer.Args tArgs = new TServer.Args(serverTransport);
+        tArgs.processor(tprocessor);
+        tArgs.protocolFactory(new TBinaryProtocol.Factory());
+        TServer server = new TSimpleServer(tArgs);
+        server.serve();
+    }
+}
