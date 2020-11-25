d459b5b1c95f173e1ff5c55b537e4ba465ed368c
76775081@qq.com <5211314a>
2020-11-25 08:36:24 +0800

    添加规则引擎模块

--- biz/src/main/java/com/iscas/biz/test/controller/AssertController.java
+++ biz/src/main/java/com/iscas/biz/test/controller/AssertController.java
@@ -1,7 +1,6 @@
 package com.iscas.biz.test.controller;
 
 import com.iscas.common.tools.assertion.AssertObjUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/assert")
 public class AssertController {
-    @Autowired
     @GetMapping("/test")
     public String test() {
         String str = null;
