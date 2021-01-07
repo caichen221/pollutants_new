6736daa7b998cca18f511eb2e41279043a5f0986
申 <1363071573@qq.com>
2021-01-07 15:19:09 +0800

    修改websocket用户拦截器接口

--- biz-base/src/main/java/com/iscas/base/biz/config/stomp/UserInterceptor.java
+++ biz-base/src/main/java/com/iscas/base/biz/config/stomp/UserInterceptor.java
@@ -2,10 +2,12 @@ package com.iscas.base.biz.config.stomp;
 
 import com.auth0.jwt.interfaces.Claim;
 import com.iscas.base.biz.config.Constants;
+import com.iscas.base.biz.service.common.SpringService;
 import com.iscas.base.biz.util.SpringUtils;
 import com.iscas.templet.exception.ValidTokenException;
 import com.iscas.base.biz.model.auth.User;
 import com.iscas.base.biz.util.JWTUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -24,42 +26,49 @@ import java.util.Map;
  *用户拦截器
  **/
 public class UserInterceptor implements ChannelInterceptor {
+
+  private  UserAccessor userAccessor = SpringService.getApplicationContext().getBean(UserAccessor.class);
+
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
-            if (raw instanceof Map) {
-                //这里就是token
-                Object name = ((Map) raw).get(Constants.TOKEN_KEY);
-                if (name instanceof List) {
-                    // 设置当前访问器的认证用户
-//                    String token = ((List) name).get(0).toString();
-//                    String username = null;
-//                    try {
-//                        Map<String, Claim> claimMap = JWTUtils.verifyToken(token);
-//                        username = claimMap.get("username").asString();
-//                        if(username == null){
-//                            throw new RuntimeException("websocket认证失败");
-//                        }
-//                    } catch (UnsupportedEncodingException e) {
-//                        e.printStackTrace();
-//                        throw new RuntimeException("websocket认证失败", e);
-//                    } catch (ValidTokenException e) {
-//                        e.printStackTrace();
-//                        throw new RuntimeException("websocket认证失败", e);
-//                    }
-//                    User user = new User();
-//                    user.setUsername(username);
-//                    accessor.setUser(user);
-
-                    User user = new User();
-                    user.setUsername(((List) name).get(0).toString());
-                    accessor.setUser(user);
-
-                }
+            if (userAccessor == null){
+                userAccessor = new DefaultUserAccessor();
             }
+            userAccessor.accessor(message,accessor);
+//            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
+//            if (raw instanceof Map) {
+//                //这里就是token
+//                Object name = ((Map) raw).get(Constants.TOKEN_KEY);
+//                if (name instanceof List) {
+//                    // 设置当前访问器的认证用户
+////                    String token = ((List) name).get(0).toString();
+////                    String username = null;
+////                    try {
+////                        Map<String, Claim> claimMap = JWTUtils.verifyToken(token);
+////                        username = claimMap.get("username").asString();
+////                        if(username == null){
+////                            throw new RuntimeException("websocket认证失败");
+////                        }
+////                    } catch (UnsupportedEncodingException e) {
+////                        e.printStackTrace();
+////                        throw new RuntimeException("websocket认证失败", e);
+////                    } catch (ValidTokenException e) {
+////                        e.printStackTrace();
+////                        throw new RuntimeException("websocket认证失败", e);
+////                    }
+////                    User user = new User();
+////                    user.setUsername(username);
+////                    accessor.setUser(user);
+//
+//                    User user = new User();
+//                    user.setUsername(((List) name).get(0).toString());
+//                    accessor.setUser(user);
+//
+////                }
+//            }
         } else if (StompCommand.SEND.equals(accessor.getCommand())) {
             //发送数据
 
