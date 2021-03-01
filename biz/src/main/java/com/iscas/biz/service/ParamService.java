7cf4442493a0ba72e85c0048537c09a6ac6c50b0
申 <1363071573@qq.com>
2021-03-01 13:51:27 +0800

    完善参数设置功能

--- biz/src/main/java/com/iscas/biz/service/ParamService.java
+++ biz/src/main/java/com/iscas/biz/service/ParamService.java
@@ -3,6 +3,9 @@ package com.iscas.biz.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.iscas.biz.model.Param;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author lirenshen
  * @vesion 1.0
@@ -10,4 +13,9 @@ import com.iscas.biz.model.Param;
  * @since jdk1.8
  */
 public interface ParamService extends IService<Param> {
+
+    boolean deleteData(List<Object> ids);
+
+    String getParamValue(String paramKey);
+
 }
