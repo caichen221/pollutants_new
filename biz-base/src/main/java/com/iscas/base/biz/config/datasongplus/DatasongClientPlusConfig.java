3632a71bd5666bc91ef73d0ab3eb8551b40871cf
76775081@qq.com <5211314a>
2021-04-04 18:06:03 +0800

    datasongclient-plus 版本升级到0.2.1，配置方式修改为repository直接注入spring的形式

--- biz-base/src/main/java/com/iscas/base/biz/config/datasongplus/DatasongClientPlusConfig.java
+++ biz-base/src/main/java/com/iscas/base/biz/config/datasongplus/DatasongClientPlusConfig.java
@@ -1,11 +1,13 @@
 package com.iscas.base.biz.config.datasongplus;
 
 import com.iscas.datasong.client.plus.config.DetBeanPostProcessor;
+import com.iscas.datasong.client.plus.config.MyBeanDefinitionRegistryPostProcessor;
 import com.iscas.datasong.client.plus.model.DetProps;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -28,11 +30,36 @@ public class DatasongClientPlusConfig {
     private String dbServer;
     @Value("${Global.DbName}")
     private String dbName;
+    @Value("${datasong.client.plus.repository.packages}")
+    private String[] packages;
     @Autowired
     private DatasongPlusProps datasongPlusProps;
 
+    //todo 旧的配置方式
+//    @Bean
+//    public DetBeanPostProcessor detBeanPostProcessor(){
+//        log.info("------------配置datasong-client-plus----------------");
+//
+//        String dbName = this.dbName;
+//        String ip = "172.16.10.180";
+//        String port = "15680";
+//        String s = StringUtils.substringAfter(dbServer, "http://");
+//        ip = StringUtils.substringBefore(s, ":");
+//        port = StringUtils.substringAfter(s, ":");
+//
+//        DetProps detProps = new DetProps();
+//        detProps.setDbName(dbName);
+//        detProps.setIp(ip);
+//        detProps.setPort(port);
+//        detProps.setProxyType(DetProps.ProxyType.CGLIB);
+//        detProps.setBasePackages(datasongPlusProps.getPackages());
+//        log.info("------------配置datasong-client-plus结束----------------");
+//        return new DetBeanPostProcessor(detProps);
+//    }
+
+    //新的配置方式，将repository直接注册到spring
     @Bean
-    public DetBeanPostProcessor detBeanPostProcessor(){
+    public BeanDefinitionRegistryPostProcessor beanDefinitionRegistryPostProcessor(){
         log.info("------------配置datasong-client-plus----------------");
 
         String dbName = this.dbName;
@@ -47,8 +74,8 @@ public class DatasongClientPlusConfig {
         detProps.setIp(ip);
         detProps.setPort(port);
         detProps.setProxyType(DetProps.ProxyType.CGLIB);
-        detProps.setBasePackages(datasongPlusProps.getPackages());
+        detProps.setRepositoryPackages(packages);
         log.info("------------配置datasong-client-plus结束----------------");
-        return new DetBeanPostProcessor(detProps);
+        return new MyBeanDefinitionRegistryPostProcessor(detProps);
     }
 }
