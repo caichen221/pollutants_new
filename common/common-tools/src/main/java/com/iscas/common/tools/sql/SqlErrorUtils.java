9ccfef41ac021d1e90764276da1f8708e79f5cab
76775081@qq.com <5211314a>
2021-04-04 18:29:31 +0800

    datasongclient-plus 版本升级到0.2.1，配置方式修改为repository直接注入spring的形式

---
+++ common/common-tools/src/main/java/com/iscas/common/tools/sql/SqlErrorUtils.java
@@ -0,0 +1,24 @@
+package com.iscas.common.tools.sql;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author zhuquanwen
+ * @vesion 1.0
+ * @date 2021/4/1 14:36
+ * @since jdk1.8
+ */
+public class SqlErrorUtils {
+
+    public enum DatabaseType {
+        MySQL, H2, SQLServer, DB2, PostgreSQL, Oracle;
+    }
+
+//    public static boolean isDuplicateRecord(final SQLException ex, DatabaseType databaseType) {
+//        return DatabaseType.MySQL.equals(databaseType) && 1062 == ex.getErrorCode() || DatabaseType.H2.equals(databaseType) && 23505 == ex.getErrorCode()
+//                || DatabaseType.SQLServer.equals(databaseType) && 1 == ex.getErrorCode() || DatabaseType.DB2.equals(databaseType) && -803 == ex.getErrorCode()
+//                || DatabaseType.PostgreSQL.equals(databaseType) && 0 == ex.getErrorCode() || DatabaseType.Oracle.equals(databaseType) && 1 == ex.getErrorCode();
+//    }
+
+}
