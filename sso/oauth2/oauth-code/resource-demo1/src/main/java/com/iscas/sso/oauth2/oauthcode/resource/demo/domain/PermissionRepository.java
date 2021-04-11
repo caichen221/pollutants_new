58edd71a7c12a7111888b74e1508a9fb618a1650
76775081@qq.com <5211314a>
2021-04-11 10:43:13 +0800

    去除了resource中的数据库依赖

--- sso/oauth2/oauth-code/resource-demo1/src/main/java/com/iscas/sso/oauth2/oauthcode/resource/demo/domain/PermissionRepository.java
+++ sso/oauth2/oauth-code/resource-demo1/src/main/java/com/iscas/sso/oauth2/oauthcode/resource/demo/domain/PermissionRepository.java
@@ -1,9 +1,9 @@
-package com.iscas.sso.oauth2.oauthcode.resource.demo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends CrudRepository<Permission,Integer> {
- 
-}
+//package com.iscas.sso.oauth2.oauthcode.resource.demo.domain;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface PermissionRepository extends CrudRepository<Permission,Integer> {
+//
+//}
