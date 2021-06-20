0b1edabf6fe4d65a39f5387086b314ca9dae1aad
76775081@qq.com <5211314a>
2021-06-20 21:34:55 +0800

    11

--- biz-base/src/main/java/com/iscas/base/biz/config/okhttp/OkHttpProps.java
+++ biz-base/src/main/java/com/iscas/base/biz/config/okhttp/OkHttpProps.java
@@ -2,15 +2,22 @@ package com.iscas.base.biz.config.okhttp;//package com.iscas.cmi.config.okhttp;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
 
 /**
  * 自动配置属性
  **/
 @ConfigurationProperties(prefix = "okhttp")
 @Data
+@Validated
 public class OkHttpProps {
+    @Min(2000)
     private int readTimeout = 2000; //读取超时时间毫秒
+    @Min(2000)
     private int writeTimeout = 2000 ; //写数据超时时间毫秒
+    @Min(2000)
     private int connectTimeout = 2000; //连接超时时间毫秒
     private int maxIdleConnection = 15 ; //最大空闲数目
     private long keepAliveDuration = 5 ; //keep alive保持时间 分钟
