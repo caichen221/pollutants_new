da6225c29a349edf1367695a7bdeea80a48947bd
76775081@qq.com <5211314a>
2021-08-20 17:34:41 +0800

    springboot升级至2.5.4，okhttp添加两个配置参数

--- biz-base/src/main/java/com/iscas/base/biz/config/okhttp/OkHttpProps.java
+++ biz-base/src/main/java/com/iscas/base/biz/config/okhttp/OkHttpProps.java
@@ -13,16 +13,31 @@ import javax.validation.constraints.Min;
 @Data
 @Validated
 public class OkHttpProps {
+    /**读取超时时间毫秒*/
     @Min(2000)
-    private int readTimeout = 10000; //读取超时时间毫秒
-    @Min(2000)
-    private int writeTimeout = 10000 ; //写数据超时时间毫秒
-    @Min(2000)
-    private int connectTimeout = 10000; //连接超时时间毫秒
-    private int maxIdleConnection = 15 ; //最大空闲数目
-    private long keepAliveDuration = 5 ; //keep alive保持时间 分钟
+    private int readTimeout = 10000;
 
-    /**拦截器类的前限定名*/
+    /**写数据超时时间毫秒*/
+    @Min(2000)
+    private int writeTimeout = 10000 ;
+
+    /**连接超时时间毫秒*/
+    @Min(2000)
+    private int connectTimeout = 10000;
+
+    /**最大空闲数目*/
+    private int maxIdleConnection = 15;
+
+    /**keep alive保持时间 分钟*/
+    private long keepAliveDuration = 5;
+
+    /**每个域名下最多允许的请求*/
+    private long maxRequests = 100;
+
+    /**最大允许请求数*/
+    private long maxRequestsPerHost = 100;
+
+    /**拦截器类的全限定名*/
     private String interceptorClasses;
 
 
