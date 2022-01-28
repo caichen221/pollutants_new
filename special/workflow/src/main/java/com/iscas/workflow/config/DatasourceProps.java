e8d6d811d132d71670b5fd759de8926b9cbdffb3
76775081@qq.com <5211314a>
2022-01-28 14:16:22 +0800

    修复了几个BUG

---
+++ special/workflow/src/main/java/com/iscas/workflow/config/DatasourceProps.java
@@ -0,0 +1,17 @@
+package com.iscas.workflow.config;
+
+import lombok.Data;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ *
+ * @author zhuquanwen
+ * @vesion 1.0
+ * @date 2022/1/27 13:50
+ * @since jdk1.8
+ */
+@Data
+@ConfigurationProperties(prefix = "camunda.bpm.datasource")
+public class DatasourceProps extends DataSourceProperties {
+}
