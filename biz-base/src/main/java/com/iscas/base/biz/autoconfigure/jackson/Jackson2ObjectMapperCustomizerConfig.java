49db884396990f38554ad534ffc7beb65d67f9c8
zhuquanwen <76775081@qq.com>
2023-01-13 14:56:20 +0800

    将LocalDateTime序列化反序列化的统一配置添加到Jackson2ObjectMapperCustomizerConfig中，并处理Mybatis使用懒加载导致Jackson序列化报错的问题

--- biz-base/src/main/java/com/iscas/base/biz/autoconfigure/jackson/Jackson2ObjectMapperCustomizerConfig.java
+++ biz-base/src/main/java/com/iscas/base/biz/autoconfigure/jackson/Jackson2ObjectMapperCustomizerConfig.java
@@ -1,4 +1,4 @@
-package com.iscas.base.biz.config.datetime;
+package com.iscas.base.biz.autoconfigure.jackson;
 
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -16,20 +16,24 @@ import java.time.format.DateTimeFormatter;
  */
 @SuppressWarnings("unused")
 @AutoConfiguration
-public class LocalDateConverterConfig {
+public class Jackson2ObjectMapperCustomizerConfig {
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
         return builder -> {
+            // 设置时间格式化样式
             builder.simpleDateFormat(DATE_TIME_FORMAT);
+
+            // LocalDate和LocalDateTime的序列化和反序列化时间格式化样式
             builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
             builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
             builder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
             builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+
+            // 处理Mybatis懒加载后Jackson序列化报错的问题
+            builder.failOnEmptyBeans(false);
         };
     }
-
-
 }
